c8bb301d7a9fe903aadf7fa88ecc75e94d35deb8
Nikita Kaptsov <kaptsovnv@yahoo.com>
2022-05-26 21:52:42 +0400

    update creds

--- config/creditials.py
+++ config/creditials.py
@@ -4,7 +4,7 @@ SITE_LINK = "http://127.0.0.1:5000"
 
 SITE_LINK_UPLOAD_FILE = os.path.join(SITE_LINK, "api/uploadFile")
 IMG_SAVING_DIR = "loaded_images"
-MAIN_SITE_LINK = os.path.join(SITE_LINK, "login")
+MAIN_SITE_LINK = os.path.join("http://127.0.0.1:3000", "")
 LOAD_IMAGE_SITE_LINK = os.path.join(SITE_LINK, "api/downloadImage")
 GET_TOKEN_SITE_LINK = os.path.join(SITE_LINK, "api/getApiToken")
 
